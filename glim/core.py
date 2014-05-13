27be5e30edb6fb7a88a4a8e2f25b873a45305746
Aras Can Akin <aacanakin@gmail.com>
2014-05-13 22:57:24 +0300

    move model classes in services into core

--- glim/core.py
+++ glim/core.py
@@ -32,29 +32,6 @@ class Registry:
         print self.registrar
         return ""
 
-# App class is a simple dependancy injection kit plus some more functionality
-class App(object):
-
-    registrar = {}
-
-    @staticmethod
-    def bind(key, value):
-        App.registrar[key] = value
-
-    @staticmethod
-    def resolve(key):
-        return App.registrar[key]
-
-# Base conroller class that extends all the controllers
-class Controller(object):
-
-    restful = False
-
-    def __init__(self, request, response):
-        self.request = request
-        self.response = response
-        self.restful = restful
-
 # Config class
 class Config(Registry):
     pass
@@ -66,3 +43,62 @@ class Session(Registry):
 # Route class
 class Router(Registry):
     pass
+
+# App class is a simple dependancy injection kit plus some more functionality
+class App:
+
+    def __init__(self, registrar):
+        self.registrar = registrar
+
+    def bind(self, key, value):
+        App.registrar[key] = value
+
+    def resolve(self, key):
+        return App.registrar[key]
+
+# Base conroller class that extends all the controllers
+class Controller:
+
+    def __init__(self, request):
+        self.request = request
+
+# Rest controller that
+class RestController(Controller):
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+    def put(self):
+        pass
+
+    def patch(self):
+        pass
+
+    def delete(self):
+        pass
+
+# metaclass for Service class
+class DeflectToInstance(type):
+    def __getattr__(selfcls, a): # selfcls in order to make clear it is a class object (as we are a metaclass)
+        try:
+            # first, inquiry the class itself
+            return super(DeflectToInstance, selfcls).__getattr__(a)
+        except AttributeError:
+            # Not found, so try to inquiry the instance attribute:
+            return getattr(selfcls.instance, a)
+
+# SERVICES
+
+# facade that is used for saving complex
+class Service:
+    __metaclass__ = DeflectToInstance
+
+    instance = None
+
+    @classmethod
+    def boot(cls, object, configuration = {}):
+        if cls.instance is None:
+            cls.instance = object(configuration)
