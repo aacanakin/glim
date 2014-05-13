d50daddde2186d54659a4f8dbf63622311ed6d22
Aras Can Akin <aacanakin@gmail.com>
2014-05-13 22:57:52 +0300

    remove service class

--- glim/services.py
+++ glim/services.py
@@ -1,23 +1,4 @@
-# metaclass for Service class
-class DeflectToInstance(type):
-    def __getattr__(selfcls, a): # selfcls in order to make clear it is a class object (as we are a metaclass)
-        try:
-            # first, inquiry the class itself
-            return super(DeflectToInstance, selfcls).__getattr__(a)
-        except AttributeError:
-            # Not found, so try to inquiry the instance attribute:
-            return getattr(selfcls.instance, a)
-
-# facade that is used for saving complex
-class Service:
-	__metaclass__ = DeflectToInstance
-
-	instance = None
-
-	@classmethod
-	def boot(cls, object, configuration = {}):
-		if cls.instance is None:
-			cls.instance = object(configuration)	
+from glim.core import Service
 
 class Config(Service):
 	pass
