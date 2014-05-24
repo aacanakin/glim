5b59d3fcfe3a8bd2edeabbb73462f0c9e7f28cc9
Aras Can Akin <aacanakin@gmail.com>
2014-05-24 20:15:37 +0300

    change services into facades

---
+++ glim/facades.py
@@ -0,0 +1,16 @@
+from glim.core import Facade
+
+class Config(Facade):
+	pass
+
+class Session(Facade):
+	pass
+
+class Cookie(Facade):
+	pass
+
+class Router(Facade):
+	pass
+
+class Database(Facade):
+	pass
