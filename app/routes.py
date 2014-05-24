e9feffd1b022b0d321f0b643d08768d2f51ba316
Aras Can Akin <aacanakin@gmail.com>
2014-05-24 20:11:34 +0300

    add route grouping samples to implement

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,25 @@
-urls = [
-	{ '/' 		: 'BaseController.hello' },
-	{'/<name>' 	: 'BaseController.greet'},
-	{'/rest'	: 'RestfulController'}
-]
+urls = {
+    '/'         : 'BaseController.hello',
+    '/<name>'   : 'BaseController.greet',
+    '/rest'     : 'RestfulController',
+
+    # optional restful routing with
+    # 'POST /rest' : 'RestfulController',
+
+    # route filters
+    # '/' : [
+    #   'BaseController.validate', # run this before the following
+    #   'BaseController.greet' # and before  the following
+    #   'BaseController.say_goodbye'
+    # ]
+
+    # route grouping
+    # '/api' : {
+    #     '/auth' : {
+    #         '/me' : [
+    #
+    #             'BaseController'
+    #         ]
+    #     }
+    # }
+}
