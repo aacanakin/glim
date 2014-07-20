fa7ad8e9efff045b07a30adac7733dbc3ad29ba1
Aras Can Akin <aacanakin@gmail.com>
2014-07-20 21:16:32 +0300

    sync configs, add comments about configurations

--- app/config/default.py
+++ app/config/default.py
@@ -1,24 +1,25 @@
 services = [
-	'glim.services.Session',
-	'glim.services.Cookie',
+	# bunch of services to be loaded up when web server starts
+]
+
+extensions = [
+	# bunch of extensions to be loaded up when web server starts
 ]
 
 config = {
 
+	# mysql database configuration
+
 	'db' : {
-		'host' : 'localhost',
-		'schema' : 'test',
-		'user' : 'root',
+		'host'	   : 'localhost',
+		'schema'   : 'test',
+		'user' 	   : 'root',
 		'password' : '',
 	},
 
-	'session' : {
-
-	},
-
-	'cookie' : {
-
-	}
+	# app specific configurations
+	# reloader: detects changes in the code base and automatically restarts web server
+	# debugger: enable werkzeug's default debugger
 
 	'glim' : {
 		'reloader' : True,
--- app/config/development.py
+++ app/config/development.py
@@ -1,28 +1,25 @@
-facades = [
-	'Session',
-	'Cookie',	
+services = [
+	# bunch of services to be loaded up when web server starts
+]
+
+extensions = [
+	# bunch of extensions to be loaded up when web server starts
 ]
 
 config = {
 
+	# mysql database configuration
+
 	'db' : {
-
-		'default' : {
-			'driver' : 'mysql',
-			'host' : 'localhost',
-			'schema' : 'test',
-			'user' : 'root',
-			'password' : '',
-		},
+		'host'	   : 'localhost',
+		'schema'   : 'test',
+		'user' 	   : 'root',
+		'password' : '',
 	},
 
-	'session' : {
-
-	},
-
-	'cookie' : {
-
-	},
+	# app specific configurations
+	# reloader: detects changes in the code base and automatically restarts web server
+	# debugger: enable werkzeug's default debugger
 
 	'glim' : {
 		'reloader' : True,
