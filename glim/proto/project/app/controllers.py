64af55f14578ed449b9be58fefb2a47a0f64b3ba
Aras Can Akin <aacanakin@gmail.com>
2014-08-02 00:10:21 +0300

    add feature for string base responses

--- glim/proto/project/app/controllers.py
+++ glim/proto/project/app/controllers.py
@@ -1,6 +1,5 @@
 from glim.core import Controller
-from werkzeug.wrappers import Response
 
 class BaseController(Controller):
 	def hello(self):
-		return Response('Welcome to glim!')
+		return 'Welcome to glim!'
