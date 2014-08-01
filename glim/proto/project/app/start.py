d2ecdb1487cd17e4252351ad31d69cc756257e3a
Aras Can Akin <aacanakin@gmail.com>
2014-08-02 00:52:03 +0300

    comment out print

--- glim/proto/project/app/start.py
+++ glim/proto/project/app/start.py
@@ -1,6 +1,7 @@
 # before application start parameter
 def before():
 	# This function is called right before the web server starts
-	print 'before starting app'
+	# print 'before starting app'
+
 	# IoC bindings may be used here
 	pass
