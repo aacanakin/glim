dbca979992154385b549748f356cf42cdf08a620
Aras Can Akin <aacanakin@gmail.com>
2014-08-03 23:23:20 +0300

    comment out db config

--- glim/proto/project/app/config/default.py
+++ glim/proto/project/app/config/default.py
@@ -21,16 +21,16 @@ config = {
 	},
 
 	# database configuration
-	'db' : {
-		'default' : {
-			'driver' : 'mysql',
-			'host' : 'localhost',
-			'schema' : 'test',
-			'user' : 'root',
-			'password' : '',
-		},
-	},
-
+	# 'db' : {
+	# 	'default' : {
+	# 		'driver' : 'mysql',
+	# 		'host' : 'localhost',
+	# 		'schema' : 'test',
+	# 		'user' : 'root',
+	# 		'password' : '',
+	# 	},
+	# },
+	
 	# app specific configurations
 	# reloader: detects changes in the code base and automatically restarts web server
 	# debugger: enable werkzeug's default debugger
