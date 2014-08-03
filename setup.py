92af6fd8f948cd563e1d327fa5ee3e58f2d26aae
Aras Can Akin <aacanakin@gmail.com>
2014-08-03 23:42:23 +0300

    add setup files for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'glim',
+  packages = ['glim'], # this must be the same as the name above
+  version = '0.7.8',
+  description = 'An elegant mvc(s) framework',
+  author = 'Aras Can Akin',
+  author_email = 'aacanakin@gmail.com',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/aacanakin/glim/tarball/0.7.8',
+# I'll explain this in a second
+  keywords = ['mvc', 'framework', 'api', 'werkzeug', 'SQLAlcehmy', 'click'], # arbitrary keywords
+  classifiers = [],
+)
