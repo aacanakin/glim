26daf9162595f41c1fc53c178d6a1c183c12cc9d
Aras Can Akin <aacanakin@gmail.com>
2014-08-03 22:24:04 +0300

    fix a bug that prevents json dump in welcome msg

--- glim/proto/project/app/controllers.py
+++ glim/proto/project/app/controllers.py
@@ -1,6 +1,10 @@
 from glim.component import Controller
 from glim.facades import View
+# import json
 
 # class BaseController(Controller):
 # 	def hello(self):
-# 		return View.render('hello', name = 'Aras')
+# 		return json.dumps({
+# 			'error' : None,
+# 			'msg' : 'Welcome to glim!'
+# 		})
