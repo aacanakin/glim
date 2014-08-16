a2e9cecc7b03db68152e69977408588e53cf1e05
Aras Can Akin <aacanakin@gmail.com>
2014-08-17 00:56:16 +0300

    add feature for custom commands

--- glim.py
+++ glim.py
@@ -2,45 +2,33 @@
 
 from termcolor import colored
 from glim.app import App
-# glim with use of click
-import click
-import shutil, errno
-import os
+from glim.utils import import_module
+from glim.command import CommandAdapter
+import glim.commands
+import traceback
+import argparse
 
-@click.group()
-def glim():
-    pass
+description = "glim (0.6.7) ~ an elegant python framework for the web"
 
-@click.command()
-@click.option('--host', default = '127.0.0.1', help = 'enter ip')
-@click.option('--port', default = '8080', help = 'enter port')
-@click.option('--env', default = 'development', help = 'enter environment (development)')
-def start(host, port, env):
-    app = App(host, port, env)
-    app.start()
+parser = argparse.ArgumentParser(description = description)
+parser.add_argument('--env', '-e', default = 'development', help = 'choose application environment(development)')
+subparsers = parser.add_subparsers(help = 'commands')
 
-@click.command()
-def new():
-    # resolve prototype path and its childs
-    proto_path = 'glim/proto/project'
-    cpath = os.path.dirname(os.path.realpath(__file__))
-    try:
-        copytree(proto_path, cpath)
-        print colored('Created new glim app', 'blue')
-    except:
-        print colored('App already exists', 'red')
+commandadapter = CommandAdapter(subparsers)
 
-def copytree(src, dst, symlinks=False, ignore=None):
-    for item in os.listdir(src):
-        s = os.path.join(src, item)
-        d = os.path.join(dst, item)
-        if os.path.isdir(s):
-            shutil.copytree(s, d, symlinks, ignore)
-        else:
-            shutil.copy2(s, d)
+# register glim commands
+commandadapter.register(glim.commands)
 
-glim.add_command(start)
-glim.add_command(new)
+# register app commands
+commandadapter.register(import_module('app.commands', 'commands'))
 
-if __name__ == '__main__':
-    glim()
+# parse the args
+args = parser.parse_args()
+
+# try to create the app, if not make it None
+app = None
+if args.which != 'new':
+	app = App(args.env)
+
+command = commandadapter.match(args)
+commandadapter.dispatch(command, app)
---
+++ glim/command.py
@@ -0,0 +1,75 @@
+import inspect
+
+class CommandAdapter:
+
+    def __init__(self, subparsers):
+        self.subparsers = subparsers
+        self.commands = []
+
+    def retrieve_commands(self, module):
+        commands = []
+
+        for name, obj in inspect.getmembers(module):
+            if name != 'Command' and 'Command' in name:
+                cobject = getattr(module, name)
+                commands.append(cobject)
+
+        return commands
+
+    def register(self, module):
+        if module is not None:
+            cmds = self.retrieve_commands(module)
+            commands = []
+
+            for c in cmds:
+                cmd = c(self.subparsers)
+                self.commands.append(cmd)
+
+    def match(self, args):
+        command = None
+        for c in self.commands:
+            if c.name == args.which:
+                c.args = args
+                command = c
+                break
+        return command
+
+    def is_glimcommand(self, command):
+        return isinstance(command, GlimCommand)
+
+    def dispatch(self, command, app):
+        if self.is_glimcommand(command):
+            command.run(app)
+        else:
+            command.run()
+
+class Command:
+
+    name = 'base'
+    description = 'base command'
+
+    # parser is the root parser
+    def __init__(self, subparsers):
+
+        self.subparsers = subparsers
+        self.parser = self.subparsers.add_parser(
+            self.name,
+            help = self.description
+        )
+        self.parser.set_defaults(which = self.name)
+
+        self.args = None
+        self.configure()
+
+    def configure(self):
+        pass
+
+    def add_argument(self, *args, **kwargs):
+        self.parser.add_argument(*args, **kwargs)
+
+    def run(self):
+        return
+
+class GlimCommand(Command):
+    def run(self, app):
+        pass
