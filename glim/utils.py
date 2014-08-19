f84e6f3a7ecd5464a5a90696c18cf0506aa14454
Aras Can Akin <aacanakin@gmail.com>
2014-08-19 21:51:08 +0300

    add comments to glim.utils functions

--- glim/utils.py
+++ glim/utils.py
@@ -1,6 +1,7 @@
 import shutil
 import os
 
+# function performs a parametric import statement, returns None if not found
 def import_module(module, frm):
     try:
         m = __import__(module, fromlist = [frm])
@@ -8,6 +9,7 @@ def import_module(module, frm):
     except Exception, e:
         return None
 
+# function performs a recursive copy of files and folders in the filesystem
 def copytree(src, dst, symlinks = False, ignore = None):
     for item in os.listdir(src):
         s = os.path.join(src, item)
