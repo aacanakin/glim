5306e26df9784e30fa613c97b2b93de2811acf91
Aras Can Akin <aacanakin@gmail.com>
2014-09-04 23:45:39 +0300

    improve import_module to automatically detect name

--- glim/utils.py
+++ glim/utils.py
@@ -1,11 +1,21 @@
 import shutil
 import os
 import traceback
+import imp
+
+# function performs an import given full path & module
+def import_source(module, path, pass_errors = False):
+    try:
+        m = imp.load_source(module, path)
+        return m
+    except Exception, e:
+        return None
 
 # function performs a parametric import statement, returns None if not found
-def import_module(module, frm, pass_errors = False):
+def import_module(module, pass_errors = False):
+    frm = module.split('.')
     try:
-        m = __import__(module, fromlist = [frm])
+        m = __import__(module, fromlist = [frm[1]])
         return m
     except Exception, e:
         if pass_errors:
