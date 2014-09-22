0571c0828f8a39d080d804e32e937b65bfe897ef
Aras Can Akin <aacanakin@gmail.com>
2014-09-22 23:31:20 +0300

    add development.py configuration

---
+++ glim/prototype/app/config/development.py
@@ -0,0 +1,69 @@
+"""
+  Here, "development" is the environment. You can run configuration
+  using the following;
+
+  $ glim start --env development # loads the development configuration
+
+"""
+
+import os
+import glim.paths
+
+config = {
+
+    # the configurations of extensions
+    'extensions': {
+        # 'gredis' : {
+        # 	'default' : {
+        # 		'host' : 'localhost',
+        # 		'port' : '6379',
+        # 		'db'   : 0
+        # 	}
+        # }
+    },
+
+    # database configuration
+    'db': {
+        # 'default' : {
+        # 	'driver' : 'mysql',
+        # 	'host' : 'localhost',
+        # 	'schema' : 'test',
+        # 	'user' : 'root',
+        # 	'password' : '',
+        # },
+    },
+
+    # the orm switch for orm to be disabled or not
+    'orm': True,
+
+    # logging configuration, it has a default configuration
+    # if you don't provide one.
+    'log': {
+        # 'level' : 'info',
+        # 'format' : '[%(levelname)s] : %(message)s',
+        # 'file' : 'app/storage/logs/debug.log'
+    },
+
+    # view configuration
+    'views': {
+        # package to be loaded by jinja2
+        'package': 'app.views'
+    },
+
+    # werkzeug sessions configuration
+    'sessions': {
+        # session id prefix
+        'id_header': 'glim_session',
+        'path': glim.paths.STORAGE_PATH,
+    },
+
+    # the glim.app.App configuration
+    'app': {
+        'reloader': True,
+        'debugger': True,
+        'static': {
+            'path': glim.paths.STATIC_PATH,
+            'url': '/static'
+        }
+    }
+}
