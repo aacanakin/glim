066ed33f074bb1693068f98c88cedfdec4241303
Aras Can Akin <aacanakin@gmail.com>
2014-11-07 19:32:16 +0200

    remove Session & Cookie facades, fix code style

--- glim/facades.py
+++ glim/facades.py
@@ -13,33 +13,21 @@ Config.get('hello') # calls the function of glim.core.Config
 
 from glim.core import Facade, Config as config, IoC as ioc
 from glim.db import Database as database, Orm as orm
-from glim.component import View as view
+from glim.view import View as view
 from glim.log import Log as log
 
 
 class Config(Facade):
     accessor = config
 
-
-class Session(Facade):
-    pass
-
-
-class Cookie(Facade):
-    pass
-
-
 class Database(Facade):
     accessor = database
-
 
 class Orm(Facade):
     accessor = orm
 
-
 class IoC(Facade):
     accessor = ioc
-
 
 class View(Facade):
     accessor = view
