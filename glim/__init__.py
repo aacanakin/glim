135d5dece60484d1dff358157cc1a9f4d38b1cee
Aras Can Akin <aacanakin@gmail.com>
2014-11-07 19:58:19 +0200

    add shortcut modules to __init__.py

--- glim/__init__.py
+++ glim/__init__.py
@@ -1,1 +1,22 @@
 version = "0.8.6"
+
+from glim import facades
+from glim import core
+from glim import controller
+from glim import ext
+from glim import service
+
+# package shortcuts for easy import
+Config = facades.Config
+Database = facades.Database
+Orm = facades.Orm
+IoC = facades.IoC
+View = facades.View
+Log = facades.Log
+
+Response = core.Response
+
+Controller = controller.Controller
+RestController = controller.RestController
+Extension = ext.Extension
+Service = service.Service
