1420ec782d8f0e0dede7bf8a21f440b0c288f58a
Aras Can Akin <aacanakin@gmail.com>
2014-11-11 22:48:54 +0200

    refactor copytree function in utils, add exception raising

--- glim/utils.py
+++ glim/utils.py
@@ -11,6 +11,8 @@ import os
 import traceback
 import imp
 
+from exception import FolderExistsError
+
 # function performs an import given full path & module
 
 
@@ -104,10 +106,13 @@ def copytree(src, dst, symlinks=False, ignore=None):
     """
     if not os.path.exists(dst):
         os.mkdir(dst)
-    for item in os.listdir(src):
-        s = os.path.join(src, item)
-        d = os.path.join(dst, item)
-        if os.path.isdir(s):
-            shutil.copytree(s, d, symlinks, ignore)
-        else:
-            shutil.copy2(s, d)
+        for item in os.listdir(src):
+            s = os.path.join(src, item)
+            d = os.path.join(dst, item)
+            if os.path.isdir(s):
+                shutil.copytree(s, d, symlinks, ignore)
+            else:
+                shutil.copy2(s, d)
+    else:
+        raise FolderExistsError("Folder already exists in %s" % dst)
+
